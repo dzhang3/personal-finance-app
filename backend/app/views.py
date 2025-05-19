1255297fff472e7b637869ebaa2a318760e6306d
Daniel Zhang <danzhang2002@gmail.com>
2025-05-19 18:05:09 -0400

    has_more updated

--- backend/app/views.py
+++ backend/app/views.py
@@ -281,21 +281,25 @@ def force_transaction_sync(request):
 
 def update_transactions_and_accounts_for_access_token(access_token, user):
     try:
-        response = send_transaction_request(access_token)
-        if 'error' in response:
-            print('Error in transaction sync:', response['error'])
-            return False
-        accounts = response['accounts']
-        print(response)
+        while True:
+            response = send_transaction_request(access_token)
+            if 'error' in response:
+                print('Error in transaction sync:', response['error'])
+                return False
+            accounts = response['accounts']
 
-        print('updating accounts')
-        db.update_accounts(user, access_token, accounts)
-        
-        print('updating cursor')
-        db.update_cursor(user, access_token, response['next_cursor'])
-        
-        print('updating transactions')
-        db.update_transactions(user, response)
+            print('updating accounts')
+            db.update_accounts(user, access_token, accounts)
+            
+            print('updating cursor')
+            db.update_cursor(user, access_token, response['next_cursor'])
+            
+            print('updating transactions')
+            db.update_transactions(user, response)
+
+            if response['has_more'] == False:
+                print('No more transactions to sync')
+                break
         
     except Exception as e:
         print('Error processing access token:', str(e))
